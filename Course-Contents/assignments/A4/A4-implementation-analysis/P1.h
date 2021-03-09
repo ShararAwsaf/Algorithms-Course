b8da015dda36378f6ea70c2b5443ba95c26862da
ShararAwsaf <sharar@uoguelph.ca>
2021-03-09 00:52:12 -0500

    Primary Inspections shows DP works

--- Course-Contents/assignments/A4/A4-implementation-analysis/P1.h
+++ Course-Contents/assignments/A4/A4-implementation-analysis/P1.h
@@ -2,6 +2,7 @@
 # include <ctype.h>
 # include <sys/time.h>
 # include <stdlib.h>
+# include <math.h>
 
 # ifndef UTILS_H
 # include "utils.h"
@@ -13,7 +14,7 @@
 // Tree Data Structure
 typedef struct treeNode
 {
-    Item item;
+    char string[MAX_WORD_SIZE];
     struct treeNode* leftSubtree;
     struct treeNode* rightSubtree;
 } TreeNode;
@@ -29,24 +30,25 @@ typedef struct tableCell
 void p11(Item* items, int n);
 
 // helpers
-void printBinaryTree(TreeNode* tree);
-void printTreeNode(TreeNode node);
+void printTreeInOrder(TreeNode* tree);
+void printTreeNode(TreeNode* node);
 void printTableCell(TableCell cell);
 void printTable(int rows, int cols, TableCell** table);
+TreeNode* createTreeNode(char* string, TreeNode* leftSubTree, TreeNode* rightSubTree);
 
 // create cost table with associated roots
 // assumes items are 1 indexed [1..n], i.e. size N = n+1
 // Table has rows: n+2 and cols: n+1
 // ROWS: [0..n+1] rows of which [1..n+1] are useful 
 // COLS: [0..n] columns
-void createCostTable(Item* items, int N, TableCell* costTable, int* rows, int* cols);
+void createCostTable(Item* items, int N, TableCell** costTable, int* rows, int* cols);
 
 // creates optimum binary search tree from items provided 
 // assumes items are 1 indexed
-TreeNode findSubtreeRecursive(int L, int R, TableCell* costTable, int rows, int cols);
-void createTreeFromTable(Item* items, int N, TableCell* costTable, int* rows, int* cols, TreeNode* obst);
-void createOBST(Item* items, int N, TreeNode* obst);
+TreeNode* findSubtreeRecursive(Item* items, int N, int L, int R, TableCell** costTable, int rows, int cols);
+TreeNode* createTreeFromTable(Item* items, int N, TableCell** costTable, int rows, int cols);
+TreeNode* createOBST(Item* items, int N);
 
 // search tree for item, print steps along the way
 // return 1 for find 0 for not found
-int searchOBST(Item searchItem, TreeNode* obst);
+int searchOBST(char* searchItem, TreeNode* obst);
--- Course-Contents/assignments/A4/A4-implementation-analysis/P11.c
+++ Course-Contents/assignments/A4/A4-implementation-analysis/P11.c
@@ -10,37 +10,57 @@ void p11(Item* items, int n)
     // printItems(items, n); // items is 1 indexed
     
     TreeNode* obst = NULL;
-    createOBST(items, n, obst);
+    obst = createOBST(items, n);
+    // TreeNode* tmpRoot = obst;
+    printTreeInOrder(obst);
 
+    char s[] = "undergraduate\0";
+    int r = searchOBST(s, obst);
+    
+    printf("'%s' ", s);
+    if(r == 0)
+        printf("found\n");
+    else
+        printf("not found (%d)\n", r);
 
 }
 
-void createOBST(Item* items, int N, TreeNode* obst)
+TreeNode* createOBST(Item* items, int N)
 {
     
-    printItem(items[1]);
     // sort items
     qsort(items, N, sizeof(Item), compareItem);
-    printItems(items, N); // items is 1 indexed
+    // printItems(items, N); // items is 1 indexed
 
     // create table
     TableCell** costTable = malloc(sizeof(TableCell*)*MAX_TABLE_SIZE);
-    int n = N-1;
-    int rows = n+2;
-    int cols = n+1;
+    int rows = 0;
+    int cols = 0;
+    createCostTable(items, N, costTable, &rows, &cols);
+    // printf("COST-TABLE (DP)\n");
+    // printTable(rows, cols, costTable);
+    
+    return createTreeFromTable(items, N, costTable, rows, cols);
+}
 
-    for(int i=0; i<rows; i++)
+void createCostTable(Item* items, int N, TableCell** costTable, int* rows, int* cols)
+{
+    int n = N-1;
+    (*rows) = n+2;
+    (*cols) = n+1;
+
+    for(int i=0; i<(*rows); i++)
     {
         costTable[i] = malloc(sizeof(TableCell)*MAX_TABLE_SIZE);
 
-        for(int j=0; j<cols; j++)
+        for(int j=0; j<(*cols); j++)
         {
             costTable[i][j].cost = 0;
             costTable[i][j].root = -1;
         }
     }
     
-    // // initialize table
+    // initialize table
     for(int i=1; i< n+1; i++)
     {
         
@@ -50,19 +70,77 @@ void createOBST(Item* items, int N, TreeNode* obst)
         costTable[i][i].cost = items[i].freq;
         costTable[i][i].root = i;
 
-        // printf("i: %d | ", i);
-        // printTableCell(costTable[i][i]);
-        // printf("\nExpected: ");
-        // printItem(items[i]);
-
-
     }
     costTable[n+1][n].cost=0;
     costTable[n+1][n].root=-1 ;
     
-    printTable(rows, cols, costTable);
+    // printf("INITIALIZATION\n");
+    // printTable( (*rows), (*cols), costTable);
+
+    for(int d=1; d<n; d++)
+    {
+        // printf("D: %d\n", d);
+
+        for(int i=1; i<n-d+1; i++)
+        {
+            int j = d+i;
+            // printf("i: %d | j: %d\n", i ,j);
+            int min_cost = INT32_MAX;
+            int min_root = -1;
+
+            for(int l=i; l<j+1; l++)
+            {
+                int cost = costTable[i][l-1].cost + costTable[l+1][j].cost;
+
+                if (cost < min_cost)
+                {
+                    min_cost = cost;
+                    min_root = l;
+                }
+            }
+
+            int W = 0;
+            for(int s=i; s<j+1; s++)
+            {
+                W += items[s].freq;
+            }
+
+            costTable[i][j].cost = min_cost + W;
+            costTable[i][j].root = min_root;
+
+        }
+
+    }
+
 }
 
+TreeNode* findSubtreeRecursive(Item* items, int N, int L, int R, TableCell** costTable, int rows, int cols)
+{
+    int R_lr = costTable[L][R].root;
+
+    TreeNode* node = createTreeNode(items[R_lr].data, NULL, NULL);
+
+    // printf("L:%d R:%d\n", L, R);
+    // printf("Going Left @ %s\n", node->string);
+    if (R_lr-1 >= L)
+        node->leftSubtree = findSubtreeRecursive(items, N, L, R_lr-1, costTable, rows, cols);
+
+    // printf("Going Right @ %s\n", node->string);
+    if (R >= R_lr+1)
+        node->rightSubtree = findSubtreeRecursive(items, N, R_lr+1, R, costTable, rows, cols);
+
+    // printf("Returning @ %s\n", node->string);
+    return node;    
+}
+
+TreeNode* createTreeFromTable(Item* items, int N, TableCell** costTable, int rows, int cols)
+{
+    return findSubtreeRecursive(items, N, 1, N-1, costTable, rows, cols);
+
+}
+
+
+// Helpers
 void printTableCell(TableCell cell)
 {
     // printf("Cost: %ld | Root: %d : ", cell.cost, cell.root);
@@ -72,7 +150,8 @@ void printTableCell(TableCell cell)
 
 void printTable(int rows, int cols, TableCell** table)
 {
-    for(int r=0; r<rows; r++)
+    // skips first row as it is not of any use
+    for(int r=1; r<rows; r++)
     {
         printf("%d) ", r);
         for(int c=0; c<cols; c++)
@@ -85,3 +164,77 @@ void printTable(int rows, int cols, TableCell** table)
     printf("ROWS: %d | COLS: %d\n", rows, cols);
 }
 
+TreeNode* createTreeNode(char* string, TreeNode* leftSubTree, TreeNode* rightSubTree)
+{
+    TreeNode* node = malloc(sizeof(TreeNode));
+    strcpy(node->string, string);
+    node->leftSubtree = leftSubTree;
+    node->rightSubtree = rightSubTree;
+
+    return node;
+}
+
+void printTreeNode(TreeNode* node)
+{
+    char d[MAX_WORD_SIZE];
+    char l[MAX_WORD_SIZE];
+    char r[MAX_WORD_SIZE];
+
+    strcpy(d, "*");
+    strcpy(l, "*");
+    strcpy(r, "*");
+    
+
+    if(node != NULL && strlen(node->string))
+        strcpy(d, node->string);
+
+    if(node->leftSubtree != NULL && strlen(node->leftSubtree->string))
+        strcpy(l, node->leftSubtree->string);
+
+    if(node->rightSubtree != NULL && strlen(node->rightSubtree->string))
+        strcpy(r, node->rightSubtree->string);
+
+    printf("     %s\n%s          %s\n", d, l, r);
+}
+
+void printTreeInOrder(TreeNode* tree)
+{
+
+    if(tree != NULL)
+    {
+        printTreeNode(tree);
+        printTreeInOrder(tree->leftSubtree);
+        printTreeInOrder(tree->rightSubtree);
+    }
+    
+}
+
+int searchOBST(char* searchItem, TreeNode* obst)
+{
+    if(obst == NULL)
+    {
+        return -5;
+    }
+
+    int compare_result = strcmp(obst->string, searchItem);
+    printf("Compared with '%s', ", obst->string);
+    
+    if(compare_result > 0)
+    {
+        printf("go to left subtree\n");
+        compare_result = searchOBST(searchItem, obst->leftSubtree);
+
+        
+    }
+    else if (compare_result < 0)
+    {
+        printf("go to right subtree\n");
+        compare_result = searchOBST(searchItem, obst->rightSubtree);
+    }
+    else
+    {
+        printf("found '%s'\n", searchItem);
+    }
+    
+    return compare_result;
+}
